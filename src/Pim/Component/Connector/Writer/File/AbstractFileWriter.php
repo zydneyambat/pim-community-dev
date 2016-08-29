ec4bbc64264ba9f60ee04be85a50e92816615ac1
willy-ahva <willy.mesnage@akeneo.com>
2016-08-29 10:01:27 +0200

    PIM-5947: Add default filepath for export

--- src/Pim/Component/Connector/Writer/File/AbstractFileWriter.php
+++ src/Pim/Component/Connector/Writer/File/AbstractFileWriter.php
@@ -23,6 +23,9 @@ abstract class AbstractFileWriter implements ItemWriterInterface, StepExecutionA
     /** @var Filesystem */
     protected $localFs;
 
+    /** @var string Datetime format for the file path placeholder */
+    protected $datetimeFormat = 'Y-m-d_H-i-s';
+
     public function __construct()
     {
         $this->localFs = new Filesystem();
@@ -33,11 +36,25 @@ abstract class AbstractFileWriter implements ItemWriterInterface, StepExecutionA
      *
      * @return string
      */
-    public function getPath()
+    public function getPath(array $placeholders = [])
     {
         $parameters = $this->stepExecution->getJobParameters();
+        $filePath = $parameters->get('filePath');
 
-        return $parameters->get('filePath');
+        if (false !== strpos($filePath, '%')) {
+            $defaultPlaceholders = ['%datetime%' => date($this->datetimeFormat), '%jobLabel%' => ''];
+            $jobExecution = $this->stepExecution->getJobExecution();
+
+            if (isset($placeholders['%jobLabel%'])) {
+                $placeholders['%jobLabel%'] = $this->sanitize($placeholders['%jobLabel%']);
+            } elseif (null !== $jobExecution->getJobInstance()) {
+                $defaultPlaceholders['%jobLabel%'] = $this->sanitize($jobExecution->getJobInstance()->getLabel());
+            }
+            $replacePairs = array_merge($defaultPlaceholders, $placeholders);
+            $filePath = strtr($filePath, $replacePairs);
+        }
+
+        return $filePath;
     }
 
     /**
@@ -46,5 +63,17 @@ abstract class AbstractFileWriter implements ItemWriterInterface, StepExecutionA
     public function setStepExecution(StepExecution $stepExecution)
     {
         $this->stepExecution = $stepExecution;
+    }
+
+    /**
+     * Replace [^A-Za-z0-9\.] from a string by '_'
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    protected function sanitize($value)
+    {
+        return preg_replace('#[^A-Za-z0-9\.]#', '_', $value);
     }
 }
