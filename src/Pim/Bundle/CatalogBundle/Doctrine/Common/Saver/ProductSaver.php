98b52ae2d198b468d12385ce926e588f1bddbe11
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2016-12-12 11:54:47 +0100

    [DISABLE FEATURE] Disable completeness calculation during save of the product

--- src/Pim/Bundle/CatalogBundle/Doctrine/Common/Saver/ProductSaver.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/Common/Saver/ProductSaver.php
@@ -57,7 +57,7 @@ class ProductSaver implements SaverInterface, BulkSaverInterface
 
         $this->eventDispatcher->dispatch(StorageEvents::PRE_SAVE, new GenericEvent($product, $options));
 
-        $this->completenessManager->schedule($product);
+        //$this->completenessManager->schedule($product);
 
         $this->objectManager->persist($product);
         $this->objectManager->flush();
@@ -85,7 +85,7 @@ class ProductSaver implements SaverInterface, BulkSaverInterface
 
             $this->eventDispatcher->dispatch(StorageEvents::PRE_SAVE, new GenericEvent($product, $options));
 
-            $this->completenessManager->schedule($product);
+            //$this->completenessManager->schedule($product);
 
             $this->objectManager->persist($product);
         }
@@ -93,7 +93,7 @@ class ProductSaver implements SaverInterface, BulkSaverInterface
         $this->objectManager->flush();
 
         foreach ($products as $product) {
-            $this->completenessManager->generateMissingForProduct($product);
+            //$this->completenessManager->generateMissingForProduct($product);
 
             $this->eventDispatcher->dispatch(StorageEvents::POST_SAVE, new GenericEvent($product, $options));
         }
